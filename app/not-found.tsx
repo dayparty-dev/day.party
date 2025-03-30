42fab5b6ea2e007cb5092ebcc603aaba19edbb82
Hmg <HmgDevWorks@gmail.com>
2025-03-30 22:49:16 +0200

    🐛 (themeSwitcher): Added custom 404 pages to resolve issues with ssr

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Link from "next/link";
+
+export default function NotFound() {
+    return (
+        <div className="w-full h-screen flex flex-col justify-center text-center">
+            <h2 className="text-xl font-bold">Not found :(</h2>
+            <Link href="/">Home</Link>
+        </div>
+    );
+}
